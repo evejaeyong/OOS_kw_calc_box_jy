ad3af6877d5e679a3f3871b19c80075317f8ca76
evejaeyong <evejaeyong@office.kw.ac.kr>
2023-10-27 10:45:55 +0900

    clone

--- main.test.js
+++ main.test.js
@@ -1,5 +1,6 @@
 const { test, expect } = require("@jest/globals");
 const { spawn } = require("child process");
+
 test("Insufficient params", () => {
     const main = spawn("node", ["main.js", "avg"]); 
     const outputs = [];
@@ -11,13 +12,18 @@ test("Insufficient params", () => {
     main.stdout.on("end", () => {
         const output = outputs.join("").trim();
         expect(output).toBe("Insufficient parameter!");
-    }); });
+    });
+});
 
-    test("Wrong command", () => {
-        const main = spawn("node", ["main.js", "sum", "1", "2", "3"]); const outputs = [];
-        main.stdout.on("data", (output) => {
-        outputs.push(output); });
-        main.stdout.on("end", () => {
+test("Wrong command", () => {
+    const main = spawn("node", ["main.js", "sum", "1", "2", "3"]);
+    const outputs = [];
+    
+    main.stdout.on("data", (output) => {
+        outputs.push(output);
+    });
+    
+    main.stdout.on("end", () => {
         const output = outputs.join("").trim(); expect(output).toBe("Wrong command!");
     });
 });
