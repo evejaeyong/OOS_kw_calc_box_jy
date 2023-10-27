c6b89ecf9335367f4a39cac503e7254ed9c6fae3
evejaeyong <evejaeyong@office.kw.ac.kr>
2023-10-27 17:34:22 +0900

    prime, fact

--- lib.js
+++ lib.js
@@ -4,13 +4,25 @@ function avg(numbers) {
 }
 
 function prime(num) {
+    let ch = true;
 
-    return false;
+    for(let i = 2; i < num; i++) {
+        if (num % i == 0) ch = false;
+    }
+
+    if (ch) return true;
+    else return false;
 }
 
 function fact(num) {
-    
-    return -1;
+    if (num > 15) return -1;
+    else {
+        let result = 1;
+        for (let i = 2; i <= num; i++) {
+            result *= i;
+        }
+        return result;
+    }
 }
 
 module.exports = {
